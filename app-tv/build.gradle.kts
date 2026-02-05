692c2ec52c8d7b08c6f8f4ee15d3f0d5781faa3b
azazellj <azazellj@gmail.com>
2026-02-06 01:27:16 +0200

    - added android tv sample app

---
+++ app-tv/build.gradle.kts
@@ -0,0 +1,49 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.compose.compiler)
+}
+
+android {
+    namespace = "dev.sdkforge.template.tv"
+    compileSdk = 36
+    defaultConfig {
+        applicationId = "dev.sdkforge.template.android"
+        minSdk = 23
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildFeatures {
+        compose = true
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+}
+
+tasks.withType<KotlinJvmCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_21)
+    }
+}
+
+dependencies {
+    implementation(projects.appShared)
+
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.appcompat)
+}
